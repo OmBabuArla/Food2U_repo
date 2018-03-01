0c4337d83a994dc24c19ffcbd84f6a1f4e3de9d5
Jatinder-Kumar-yadav <jkyadav11404572@gmail.com>
2018-03-01 15:26:31 +0530

    Admin Customer and Vendor Dashboard Added Completed

--- app/src/main/java/com/example/omarla/food2u_repo/AdminDashborad.java
+++ app/src/main/java/com/example/omarla/food2u_repo/AdminDashborad.java
@@ -13,13 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class VendorDashboard extends AppCompatActivity
+public class AdminDashborad extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vendor_dashboard);
+        setContentView(R.layout.activity_admin_dashborad);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -55,7 +55,7 @@ public class VendorDashboard extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.vendor_dashboard, menu);
+        getMenuInflater().inflate(R.menu.admin_dashborad, menu);
         return true;
     }
 
